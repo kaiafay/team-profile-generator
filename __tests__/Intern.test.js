78354583871efc742fbfd3b98c345ef3e6f757a9
Kaia Scheirman <kfscheirman18@gmail.com>
2022-04-02 13:11:32 -0700

    create all tests for intern class

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,25 @@
+const Intern = require('../lib/Intern.js');
+
+test('set school', () => {
+    const school = 'school';
+    // placeholder for name, id, and email
+    const intern = new Intern('Foo', 1, 'email@email.com', school);
+
+    expect(intern.school).toBe(school);
+});
+
+// make sure getSchool() method returns intern school
+test("get intern's school", () => {
+    const school = 'school';
+    const intern = new Intern('Foo', 1, 'email@email.com', school);
+
+    expect(intern.getSchool()).toBe(school);
+});
+
+// make sure getRole() metho returns intern role
+test("get intern's role", () => {
+    const role = 'Intern'
+    const intern = new Intern('Foo', 1, 'email@email.com', 'school');
+
+    expect(intern.getRole()).toBe(role);
+});
