d75c43c30525d21fe10ae9f8585e15a37e4782f5
Kaia Scheirman <kfscheirman18@gmail.com>
2022-04-02 13:30:22 -0700

    import necessary modules and files

---
+++ index.js
@@ -0,0 +1,8 @@
+// import team member files
+const Employee = require('./lib/Employee.js');
+const Engineer = require('./lib/Engineer.js');
+const Intern = require('./lib/Intern.js');
+const Manager = require('./lib/Manager.js');
+// import fs and path modules
+const fs = require('fs');
+const path = require('path');
