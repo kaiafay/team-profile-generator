a0263be50ba8febf1a30c4b8ad3fa5f6cf3caf9c
Kaia Scheirman <kfscheirman18@gmail.com>
2022-04-06 17:35:04 -0700

    export html page

--- src/page-template.js
+++ src/page-template.js
@@ -79,3 +79,40 @@ const generatePage = team => {
         // returns html array as a joined string
         return html.join("");
 };
+
+// export function to generate html page
+module.exports = team => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>My Team</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    </head>
+
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 jumbotron mb-3 team-heading">
+                    <h1 class="text-center">My Team</h1>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="team-area col-12 d-flex justify-content-center">
+                    ${generatePage(team)}
+                </div>
+            </div>
+        </div>
+
+        <script src="https://kit.fontawesome.com/c502137733.js"></script>
+    </body>
+    </html> 
+    `;
+};
